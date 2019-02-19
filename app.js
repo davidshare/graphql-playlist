07c79ced9e7c7cfb9dd7c78f60a211e3b84f797f
davidshare <davidessienshare@gmail.com>
2019-02-19 19:29:06 +0100

    feat(bookQueries): add queries for books and authors
    - add a query for getting books
    - add a query for getting authors
    - install lodash

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./server/schema/schema');
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-
+	schema,
+	graphiql: true
 }));
 
 app.listen(4000, ()=> console.log('app listening on port 4000'));
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,75 @@
 const graphql = require('graphql');
+const _= require('lodash');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLSchema,
+	GraphQLID,
+	GraphQLInt
+} = graphql;
+
+//dummy data
+const books = [
+	{name: 'king david', genre: 'gospel', id:'1'},
+	{name: 'queen faith', genre: 'drama', id:'2'},
+	{name: 'queen marvey', genre: 'drama', id:'3'},
+]
+
+const authors = [
+	{name: 'David Essien', age: '30', id:'1'},
+	{name: 'Faith Obeten', age: '28', id:'2'},
+	{name: 'Marvelous Ikpi', age: '21', id:'3'},
+]
 
 const BookType = new GraphQLObjectType({
 	name: 'Book',
 	fields: () => ({
-		id: { type: GraphQLString },
+		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		genre: { type: GraphQLString },
 	})
+});
+
+const AuthorType = new GraphQLObjectType({
+	name: 'Author',
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		age: { type: GraphQLInt },
+	})
+});
+
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields:  {
+		book: {
+			type: BookType,
+			args: {
+				id: {
+					type: GraphQLID
+				}
+			},
+			resolve(parent, args){
+				//code to get data from db
+				return _.find(books, {id: args.id});
+			}
+		},
+		author: {
+			type: AuthorType,
+			args: {
+				id: {
+					type: GraphQLID
+				}
+			},
+			resolve(parent, args){
+				//code to get data from db
+				return _.find(authors, {id: args.id});
+			}
+		}
+	}
+})
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
 });
